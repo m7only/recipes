5b94a0fe451ae59edf3baf5834715a7364c12745
Only <ongranly@ngs.ru>
2023-02-06 17:31:10 +0700

    ДЗ Структура веб-приложения

--- src/main/java/com/m7/recipes/entity/Ingredient.java
+++ src/main/java/com/m7/recipes/entity/Ingredient.java
@@ -2,9 +2,9 @@ package com.m7.recipes.entity;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
-import java.util.Objects;
-
+@Data
 public class Ingredient {
     @NotBlank
     String ingredientTitle;
@@ -13,49 +13,4 @@ public class Ingredient {
     @NotBlank
     String measurementUnit;
 
-    public String getIngredientTitle() {
-        return ingredientTitle;
-    }
-
-    public void setIngredientTitle(String ingredientTitle) {
-        this.ingredientTitle = ingredientTitle;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    public String getMeasurementUnit() {
-        return measurementUnit;
-    }
-
-    public void setMeasurementUnit(String measurementUnit) {
-        this.measurementUnit = measurementUnit;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Ingredient that = (Ingredient) o;
-        return ingredientTitle.equals(that.ingredientTitle) && quantity.equals(that.quantity) && measurementUnit.equals(that.measurementUnit);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(ingredientTitle, quantity, measurementUnit);
-    }
-
-    @Override
-    public String toString() {
-        return "Ingredient{" +
-                "ingredientTitle='" + ingredientTitle + '\'' +
-                ", quantity=" + quantity +
-                ", measurementUnit='" + measurementUnit + '\'' +
-                '}';
-    }
 }
--- src/main/java/com/m7/recipes/entity/Recipe.java
+++ src/main/java/com/m7/recipes/entity/Recipe.java
@@ -2,12 +2,12 @@ package com.m7.recipes.entity;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
-
+@Data
 public class Recipe {
     @NotBlank
     String title;
@@ -16,59 +16,4 @@ public class Recipe {
 
     List<Ingredient> ingredients;
     Map<Integer, String> instruction;
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Integer getCookTime() {
-        return cookTime;
-    }
-
-    public void setCookTime(Integer cookTime) {
-        this.cookTime = cookTime;
-    }
-
-    public List<Ingredient> getIndigrients() {
-        return ingredients;
-    }
-
-    public void setIndigrients(List<Ingredient> ingredients) {
-        this.ingredients = ingredients;
-    }
-
-    public Map<Integer, String> getInstruction() {
-        return instruction;
-    }
-
-    public void setInstruction(Map<Integer, String> instruction) {
-        this.instruction = instruction;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Recipe recipe = (Recipe) o;
-        return title.equals(recipe.title) && cookTime.equals(recipe.cookTime) && ingredients.equals(recipe.ingredients) && instruction.equals(recipe.instruction);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(title, cookTime, ingredients, instruction);
-    }
-
-    @Override
-    public String toString() {
-        return "Recipe{" +
-                "title='" + title + '\'' +
-                ", cookTime=" + cookTime +
-                ", indigrients=" + ingredients +
-                ", instruction=" + instruction +
-                '}';
-    }
 }
